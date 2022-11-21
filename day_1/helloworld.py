2da429f1cdb20b88a8047fc39602efcf15ba4ded
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-11-21 14:27:21 +0300

    Day 1 done

---
+++ day_1/helloworld.py
@@ -0,0 +1,19 @@
+# Exercise: Level 2
+# Data types examples
+
+# Number
+    # integer: 3
+    # float: 3.5
+    # complex: 3i + j
+
+# String: 'My name is Dhulkifli Abbas'
+# Boolean: True, False
+# List: [1, 2, 'Three']
+# Tuple: (1, 2, 3)
+# Set: {1, 2, 3}
+# Dictionary: {'Name': 'Dhulkifli', 'Age': 24}
+
+
+# Exercise: Level 3
+# Finding Euclidian distance between (2, 3) and (10, 8)
+print(( ((10 - 2) ** 2) + ((8 - 3) ** 2) ) ** 0.5)
